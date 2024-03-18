94406ec035740f647eb2c7996e5c6f27535bc273
David Rider <Rider.David@mayo.edu>
2024-03-18 13:38:22 -0500

    Breaking out objects into their own files

---
+++ contacts/contact.py
@@ -0,0 +1,133 @@
+import sys
+from site import SiteGroup
+from tabulate import tabulate
+from openpyxl import load_workbook
+from openpyxl.workbook.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+def clean(str):
+   if not str:
+       return ""
+   return str.strip().replace("\"", "")
+
+class Contact:
+    """OTTA contact
+
+    attributes: acronym, name, email, primary_contact
+    """
+
+    def __init__(self, acronym, name, email, primary_contact):
+        self.acronym = acronym
+        self.name = name
+        self.email = email
+        self.primary_contact = primary_contact
+
+    def primary_yes_no(self):
+        if self.primary_contact:
+            return "Yes"
+        return "No"
+
+class ContactList:
+    """List of OTTA contacts
+
+    get list of primary contacts by acronym
+    """
+
+
+    def __init__(self, otta_contacts_excel_path, public_excluded_sites_filename=None):
+        self.missing_otta_sites = False
+
+        wb: Workbook = load_workbook(filename=otta_contacts_excel_path)
+        sheet: Worksheet = wb.active
+        max_row = sheet.max_row
+
+        self.contacts = {}
+        for i in range(1, max_row + 1):
+            acronym = clean(sheet.cell(row=i, column=1).value)
+
+            primary_contact = False;
+            yes_no = sheet.cell(row=i, column=2).value
+            if (yes_no.lower() == "yes"):
+                primary_contact = True;
+
+            name = clean(sheet.cell(row=i, column=3).value)
+            email = clean(sheet.cell(row=i, column=4).value)
+
+            contact = Contact(acronym, name, email, primary_contact)
+            contact_list_for_acronym = []
+            if acronym in self.contacts:
+                contact_list_for_acronym = self.contacts[acronym]
+            contact_list_for_acronym.append(contact)
+            self.contacts[acronym] = contact_list_for_acronym
+
+        self.excluded_public_groups = self._read_excluded_public_groups(public_excluded_sites_filename)
+
+    def _read_excluded_public_groups(self, public_excluded_sites_filename):
+        if not public_excluded_sites_filename:
+            return None
+
+        excluded_groups = []
+        with open(public_excluded_sites_filename) as fp:
+            Lines = fp.readlines()
+            for line in Lines:
+                excluded_groups.append(line.strip())
+        return excluded_groups
+
+    def missing_sites(self, sites):
+        for acronym in self.contacts:
+            if (not acronym in sites.ordered_acronyms()):
+                return True
+        return False
+
+    def print_contacts_without_sites(self, sites):
+        if not self.missing_sites(sites):
+            return
+
+        print("Some OTTA sites from contact(s) are missing from master list", file=sys.stderr)
+        for acronym in self.contacts:
+            if not acronym in sites.ordered_acronyms():
+                names = []
+                for contact in self.contacts[acronym]:
+                    names.append(contact.name)
+                print("%s: %s" % (acronym, names), file=sys.stderr)
+                print()
+
+    def _add_class(self, table_html:str):
+        return table_html.replace("<table>", '<table class="otta-table">')
+
+    def print_public_table(self, sites):
+        table_data = []
+        for acronym in sites.ordered_acronyms():
+            num_primary_contacts_for_site = 0
+            if acronym in self.excluded_public_groups:
+                print(acronym + " excluded from public table", file=sys.stderr)
+                continue
+            site = sites.site_for_acronym(acronym)
+            if not acronym in self.contacts:
+                print("No contacts listed for OTTA site: " + acronym, file=sys.stderr)
+                continue
+            for contact in self.contacts[acronym]:
+                if contact.primary_contact:
+                    num_primary_contacts_for_site += 1
+                    table_data.append([acronym, site.description, contact.name, contact.email, site.ocac_acronym])
+
+            # maybe put out a warning for an OTTA site if there is no primary contact?
+            if not num_primary_contacts_for_site:
+                print("Couldn't find a primary contact for " + acronym, file=sys.stderr)
+            if num_primary_contacts_for_site > 1:
+                print("Published multiple primary contacts for " + acronym, file=sys.stderr)
+
+        table_html = tabulate(table_data, headers=["OTTA Site", "Study Name", "Contact", "Email", "OCAC"], tablefmt="html")
+        print(self._add_class(table_html))
+
+    def print_member_table(self, sites):
+        table_data = []
+        for acronym in sites.ordered_acronyms():
+            if not acronym in self.contacts:
+                print("No contacts listed for OTTA site: " + acronym)
+                continue
+            for contact in self.contacts[acronym]:
+                table_data.append([acronym, contact.primary_yes_no(), contact.name, contact.email])
+
+        table_html = tabulate(table_data, headers=["OTTA Study Acronym", "Primary Contact", "Name", "Email"], tablefmt="html")
+        print(self._add_class(table_html))
---
+++ contacts/site.py
@@ -0,0 +1,54 @@
+import sys
+from openpyxl import load_workbook
+from openpyxl.workbook.workbook import Workbook
+
+class Site:
+    """Represents an OTTA site
+
+    attributes: acronym, description, otta_acronym
+    """
+
+    def __init__(self, acronym, description, ocac_acronym = None):
+        self.acronym = acronym
+        self.description = description
+        self.ocac_acronym = ocac_acronym
+
+class SiteGroup:
+    """A group of OTTA sites
+
+    Can get a site by acronym and return a list of acronyms in order
+    """
+
+    def __init__(self, otta_site_excel_path):
+        self.map = {}
+        wb: Workbook = load_workbook(filename=otta_site_excel_path)
+        sheet = wb.active
+        max_row = sheet.max_row
+        for i in range(1, max_row + 1):
+            acronym = sheet.cell(row=i, column=1).value
+            # skip header
+            if (acronym == "OTTA Site"):
+                continue
+            if (self.has_acronym(acronym)):
+                print("Duplicate OTTA site " + acronym)
+                sys.exit(1)
+            description = sheet.cell(row=1, column=2).value
+            ocac_acronym = sheet.cell(row=i, column=3).value
+            if (ocac_acronym and ocac_acronym.strip() == ""):
+                ocac_acronym = None
+            self.map[acronym] = Site(acronym, description, ocac_acronym)
+
+    def ordered_acronyms(self):
+        # acronym_list = []
+        # for acronym in self.map.keys():
+        #     acronym_list.append(acronym)
+        # return acronym_list.sort()
+        return sorted(self.map.keys())
+
+    def has_acronym(self, acronym):
+        return acronym in self.map.keys()
+
+    def site_for_acronym(self, acronym):
+        if (acronym in self.map):
+            return self.map[acronym]
+        raise KeyError("No site for acronym " + acronym)
