0b037fd9a48eabd384eebe5a7bfda3cb1f5a1e3a
David Rider <Rider.David@mayo.edu>
2024-03-18 13:39:19 -0500

    Breaking out single program with optional params into separate programs with required params

---
+++ contacts/write_otta_public_contacts.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+from site import SiteGroup
+from contact import ContactList
+
+if len(sys.argv) < 4:
+    print("Usage: %s otta_ocac.xls contacts.xls public_excluded_sites.txt" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+
+otta_description_ocac_filename = sys.argv[1]
+sites = SiteGroup(otta_description_ocac_filename)
+
+contacts_filename = sys.argv[2]
+public_excluded_sites_filename = sys.argv[3]
+contacts = ContactList(contacts_filename, public_excluded_sites_filename)
+
+if contacts.missing_sites(sites):
+    contacts.print_contacts_without_sites(sites)
+    sys.exit(1)
+
+contacts.print_public_table(sites)
